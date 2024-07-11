3fae5a35559af8f97ec0ad97e275449b43209edb
olexandermushtyn <mushtynolexander@gmail.com>
2024-07-11 12:33:53 +0300

    Initial commit

---
+++ internal/config/config.go
@@ -0,0 +1,27 @@
+package config
+
+import (
+	"log"
+	"os"
+	"time"
+)
+
+type Config struct {
+	Env         string     `yaml:"env" env-default:"local"`
+	StoragePath string     `yaml:"storage_path" env-required:"true"`
+	HTTPServer  HTTPServer `yaml:"http"`
+}
+
+type HTTPServer struct {
+	Address     string        `yaml:"address" env-default:"localhost:8082"`
+	Timeout     time.Duration `yaml:"timeout" env-default:"4s"`
+	IdleTimeout time.Duration `yaml:"idle_timeout" env-default:"60s"`
+}
+
+func MustLoad() {
+	configPath := os.Getenv("CONFIG_PATH")
+	if configPath == "" {
+		log.Fatal("CONFIG_PATH is required")
+	}
+
+}
